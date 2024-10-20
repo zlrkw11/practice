383fb7d90adee06b3971cdd6c2e804c4beeceeeb
ZL <ray040406@gmail.com>
2024-10-20 23:58:49 +1300

    section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,13 @@
-import Image from "next/image";
-
 export default function Home() {
   return (
-    <div className="">
-      <Frame />
+    <div className="w-full flex flex-col items-center font-bold">
+      <div className="m-8 text-3xl "> Home page</div>
+      <div className="border-2 border-orange-400 rounded-md bg-yellow-200 p-4">
+        <div>GDSC hackathon</div>
+        <button>go</button>
+      </div>
     </div>
   );
 }
 
-const Frame = () => {
-  return <div>{}</div>;
-};
+const section = ({ title }: { title: string }) => {};
