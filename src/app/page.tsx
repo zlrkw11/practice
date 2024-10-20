a52b897a519a92ee5a7900a2c2db6f4bb4877b82
ZL <ray040406@gmail.com>
2024-10-20 23:59:54 +1300

    section used

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,11 +3,17 @@ export default function Home() {
     <div className="w-full flex flex-col items-center font-bold">
       <div className="m-8 text-3xl "> Home page</div>
       <div className="border-2 border-orange-400 rounded-md bg-yellow-200 p-4">
-        <div>GDSC hackathon</div>
-        <button>go</button>
+        <Section title="GDSC" />
       </div>
     </div>
   );
 }
 
-const section = ({ title }: { title: string }) => {};
+const Section = ({ title }: { title: string }) => {
+  return (
+    <div className="border-2 border-orange-400 rounded-md bg-yellow-200 p-4">
+      <h1>{title}</h1>
+      <button>go</button>
+    </div>
+  );
+};
