471b8f2ceee3530c8b10085baeee363d415a530f
ZL <ray040406@gmail.com>
2024-10-21 00:03:51 +1300

    linked

---
+++ src/app/gdsc/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <div className="w-full flex flex-col items-center font-bold">
+      <div className="m-8 text-3xl ">GDSC</div>
+    </div>
+  );
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,21 @@
+import Link from "next/link";
+
 export default function Home() {
   return (
     <div className="w-full flex flex-col items-center font-bold">
-      <div className="m-8 text-3xl "> Home page</div>
-      <div className="border-2 border-orange-400 rounded-md bg-yellow-200 p-4">
-        <Section title="GDSC" />
-      </div>
+      <div className="m-8 text-3xl ">Home page</div>
+      <Section title="GDSC" />
     </div>
   );
 }
 
 const Section = ({ title }: { title: string }) => {
   return (
-    <div className="border-2 border-orange-400 rounded-md bg-yellow-200 p-4">
+    <div className="border-2 border-orange-400 rounded-md bg-yellow-200 px-8 py-4">
       <h1>{title}</h1>
-      <button>go</button>
+      <Link href="/gdsc">
+        <button>go</button>
+      </Link>
     </div>
   );
 };
