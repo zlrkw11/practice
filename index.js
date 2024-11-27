297d3790041ba8df17cbaf9946c5d582e16b3ba5
ZL <ray040406@gmail.com>
2024-11-27 23:30:53 +1300

    fix

--- index.js
+++ index.js
@@ -40,7 +40,7 @@ app.get("/api/notes/:id", (req, res) => {
   }
 });
 
-app.get("/api/notes/:id", (req, res) => {
+app.delete("/api/notes/:id", (req, res) => {
   const id = Number(req.params.id);
   notes = notes.filter((note) => note.id !== id);
   res.status(204).end();
